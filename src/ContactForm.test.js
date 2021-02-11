b84db9ab0b6d4cd805b1f7d9b8e1981a83950600
CCooper92 <am10ac17@gmail.com>
2021-02-11 13:08:53 -0800

    can not get all my tests to work

---
+++ src/ContactForm.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ContactForm from './components/ContactForm';
+import { act } from 'react-dom/test-utils';
+
+test('renders without errors', () => {
+    render (<ContactForm />);
+});
+
+test("form is filled out and submit adds users information", () => {
+
+    //render
+    render (<ContactForm />)
+
+    //query for all inputs
+    //const emailInput = screen.getByLabelText(/email/i);
+    const firstNameInput = screen.queryByLabelText(/first name/i);
+    const lastNameInput = screen.queryByLabelText(/last name/i);
+    const emailInput = screen.queryByLabelText(/email/i);
+    const messageInput = screen.getByLabelText(/message/i);
+
+
+    //type into inputs
+    // userEvent.type(firstNameInput, "Edd");
+    // userEvent.type(firstNameInput, "Edd");
+    userEvent.type(lastNameInput, "Cooper");
+    userEvent.type(messageInput, "testing");
+    
+    //negative assertion
+
+
+    //query for the button
+    const button = screen.getByRole("button", { name: /submit/i });
+
+    //click the button
+
+        // userEvent.click(button);
+    
+ 
+  
+
+    //query for the test "Courtney"
+
+
+    //assert
+})
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { act}  from 'react-dom/test-utils';
 import "./index.css";
 import App from "./App";
 
+act(() => {
 ReactDOM.render(<App />, document.getElementById("root"));
+});
